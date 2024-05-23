90e790b5c794bc1551987cb98a3e222544b699ad
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-05-23 22:33:14 +0545

    added automation

---
+++ automate.py
@@ -0,0 +1,45 @@
+import os
+import time
+from play import click, run
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+options = webdriver.EdgeOptions()
+options.add_experimental_option('detach', True)
+
+
+os.environ['PATH'] += r'../driver'
+driver = webdriver.Edge(options=options)
+
+driver.get('https://www.gamesgames.com/game/magic-piano-tiles')
+driver.maximize_window()
+
+WebDriverWait(driver, 20).until(
+    EC.presence_of_element_located((By.ID, "onetrust-accept-btn-handler"))
+)
+driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
+
+# Wait for the iframe to be present
+iframe = WebDriverWait(driver, 20).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "iframe[src*='html5.gamedistribution.com']"))
+)
+# Switch to the iframe
+driver.switch_to.frame(iframe)
+
+# Now wait for the play button to be clickable
+play_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "pluto-splash-button")))
+play_button.click()
+
+
+# Bypass adds
+time.sleep(15)
+click(1198, 814)
+# Click the start button
+time.sleep(10)
+click(677, 516)
+
+# Let the beethoven play
+run()
+
---
+++ play.py
@@ -0,0 +1,49 @@
+import pyautogui
+import time
+import keyboard
+import win32api, win32con
+
+
+def print_mouse_coordinates():
+    while True:
+#     # Wait for the 's' key to be pressed
+        if keyboard.is_pressed('s'):
+            x, y = pyautogui.position()
+            print(f'Mouse coordinates: ({x}, {y})')
+            # Adding a short delay to avoid multiple prints on a single key press
+            keyboard.wait('s', suppress=False)
+        
+        # Break the loop and exit if 'q' is pressed
+        if keyboard.is_pressed('q'):
+            break
+    
+    
+
+(x1, y1) = (526, 550)
+(x2, y2) = (630, 546)
+(x3, y3) = (728, 547)
+(x4, y4) = (820, 547)
+    
+
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.02)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    
+def run():
+    while keyboard.is_pressed('q') == False:
+        
+        if pyautogui.pixel(x1, y1)[0] == 0:
+            click(x1, y1)
+        if pyautogui.pixel(x2, y2)[0] == 0:
+            click(x2, y2)
+        if pyautogui.pixel(x3, y3)[0] == 0:
+            click(x3, y3)
+        if pyautogui.pixel(x4, y4)[0] == 0:
+            click(x4, y4)
+        
+        
+if __name__ == '__main__':
+    print_mouse_coordinates()
+    # run()
